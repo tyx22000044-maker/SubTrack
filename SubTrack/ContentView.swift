a0705eaa9ddce94bc84544f3d6b81b7ea8be4d7f
YunxuanTan <tyx22000044@163.com>
2026-03-25 12:03:51 -0400

    Initial Commit

---
+++ SubTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubTrack
+//
+//  Created by 谈昀轩的Macbook on 2026-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubTrack/SubTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubTrackApp.swift
+//  SubTrack
+//
+//  Created by 谈昀轩的Macbook on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct SubTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
